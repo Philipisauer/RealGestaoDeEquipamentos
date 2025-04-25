0ed03ea9bbd4547a27c148a776d763f0540c723d
Philipi Sauer <philipisauer09@gmail.com>
2025-04-25 15:05:56 -0300

    Adicionar arquivos de projeto.

---
+++ RealGestaoDeEquipamentos.ConsoleApp/Equipment.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RealGestaoDeEquipamentos.ConsoleApp
+{
+    public class Equipment
+    {
+        public int Id;
+        public string Nome;
+        public string Fabricante;
+        public decimal PrecoDoProduto;
+        public DateTime DataDeFabricacao;
+
+        public Equipment(string nome, string fabricante, decimal precoDoProduto, DateTime dataDeFabricacao) 
+        {
+            Nome = nome;
+            Fabricante = fabricante;
+            PrecoDoProduto = precoDoProduto;
+            DataDeFabricacao = dataDeFabricacao;
+        }
+
+        
+        public string ObterNumeroDeSerie()
+        {
+            string tresPrimeirosLetras = Nome.Substring(0, 3);
+
+            return $"{tresPrimeirosLetras}-{Id}";
+        }
+        
+            
+    }
+}
---
+++ RealGestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace RealGestaoDeEquipamentos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe a operação desejda:");
+            Console.WriteLine("1 - cadastrar equipamentos");
+            Console.WriteLine("4 - visualizar equipamentos");
+            string chosenOption = Console.ReadLine()!;
+
+            switch (chosenOption)
+            {
+                case "1":
+                    Console.Clear();
+
+                    Console.WriteLine("Digite o nome do equipamento: ");
+                    string name = Console.ReadLine()!;
+
+                    Console.WriteLine("Digite o fabricante do equipamento: ");
+                    string fabricante = Console.ReadLine()!;
+
+                    Console.WriteLine("Digite o preço do equipamento: ");
+                    decimal valueOfProduct = Convert.ToDecimal(Console.ReadLine());
+
+                    Console.WriteLine("Digite a data de fabricação do equipamento");
+                    DateTime fabricationDate = Convert.ToDateTime(Console.ReadLine());
+
+                    Equipment equipamento = new Equipment(name, fabricante, valueOfProduct, fabricationDate);
+                    break;
+
+                default:
+                    Console.WriteLine("saindo do programa...");
+                        break;
+                }
+            Console.ReadLine();
+
+        }
+    }
+}
